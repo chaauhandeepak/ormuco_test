8839f93114bd2f726dabbffa7277faadc7a41d35
chaauhandeepak <43662452+chaauhandeepak@users.noreply.github.com>
2018-12-15 14:46:00 -0800

    Update overlapping_checking.py

--- overlapping_checking.py
+++ overlapping_checking.py
@@ -1,9 +1,9 @@
-class Point(object):
-    def __init__(self, x, y):
+class Point(object): ## class point with argument object
+    def __init__(self, x, y): 
         self.x = x
         self.y = y
 
-class Rect(object):
+class Rect(object): ##rect class with argument object
     def __init__(self, p1, p2):
         '''Store the top, bottom, left and right values for points
                p1 and p2 are the (corners) in either order
@@ -22,7 +22,7 @@ def overlap(r1,r2):
         hoverlaps = False
     if (r1.top < r2.bottom) or (r1.bottom > r2.top): 
         voverlaps = False
-    return hoverlaps and voverlaps
+    return hoverlaps and voverlaps ##returning values 
 
 if __name__ == '__main__': 
     p1 = Point(1,1)
